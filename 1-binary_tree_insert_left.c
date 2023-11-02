16324dee4d6891a5483a6939c016bca8b3ce15a5
Ella4040 <usouella@gmail.com>
2023-11-02 09:24:15 -0700

    udapte

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,28 +9,27 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    /*Checks if the parent is NULL, in which case the function cannot insert a node on the left.*/
-    if (parent == NULL)
-    {
-      return (NULL);
-    }
+	/*Checks if the parent is NULL, in which case the function cannot insert a node on the left.*/
+	if (parent == NULL)
+	{
+		return (NULL);
+	}	
 
-    /*Creates a new node with the specified value.*/
-    binary_tree_t *new = binary_tree_node(parent, value);
-    if (new == NULL)
-    {
-        return (NULL);
-    }
+	/*Creates a new node with the specified value.*/
+	binary_tree_t *new = binary_tree_node(parent, value);
+	if (new == NULL)
+	{
+	return (NULL);
+	}
 
-    /*If the parent node already has a left child, rearranges the links*/
-    if (parent->left != NULL)
-    {
-        new->left = parent->left; // The new node points to the old left child of the parent.
-        parent->left->parent = new; // Updates the parent of the old left child to point to the new node.
-    }
+	/*If the parent node already has a left child, rearranges the links*/
+	if (parent->left != NULL)
+	{
+	new->left = parent->left; /* The new node points to the old left child of the parent*/
+	parent->left->parent = new; /*Updates the parent of the old left child to point to the new nod*/
+	}
+	parent->left = new; /*Updates the left child of the parent to point to the new node*/
 
-    parent->left = new_node; /*Updates the left child of the parent to point to the new node*/
-
-    /*Return a pointer to the new node*/
-    return (new);
+	/*Return a pointer to the new node*/
+	return (new);
 }
