95b5160dcf6b1f32196338e237b1467f2e50f540
Ella4040 <usouella@gmail.com>
2023-11-02 12:10:07 -0700

    udapte

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new = binary_tree_node(parent, value);
 	if (new == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
 
 	/*If the parent node already has a left child, rearranges the links*/
