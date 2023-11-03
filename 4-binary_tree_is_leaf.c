5b35aa145516ae1be583828c1e05a0161826ff35
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-11-03 03:32:53 +0100

    Update

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf: A function that checks if a given node is a root.
+ * binary_tree_is_leaf: A function that checks if a node is a leaf.
  * @node: A pointer to the node to check.
  * Return: return 1 if node is a root otherwise 0.
  */
